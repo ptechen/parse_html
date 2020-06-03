b53d74ce83550c436b9aaaa9e8c868ec25cb9014
petechen <15736755067@163.com>
2020-06-03 19:38:12 +0800

    update

--- parse/parse.go
+++ parse/parse.go
@@ -6,30 +6,23 @@ import (
 )
 
 type FilterParams struct {
-	Selector   string                   `json:"selector"`
-	Finds      []string                 `json:"finds"`
-	TowFinds   []string                 `json:"tow_finds"`
-	ThreeFinds []string                 `json:"three_finds"`
-	Type       string                   `json:"type"`
-	Keys       map[string]*FilterParams `json:"keys"`
-	Last       bool                     `json:"last"`
-	First      bool                     `json:"first"`
-	Eq         int                      `json:"eq"`
-	HasClass   string                   `json:"has_class"`
-	Attr       string                   `json:"attr"`
-	Split      *Split                   `json:"split"`
-	Contains   string                   `json:"contains"`
-	Deletes    []string                 `json:"deletes"`
-	Replaces   []*Replace               `json:"replaces"`
-}
-
-type Tag struct {
-	Index   int    `json:"index"`
-	Contain string `json:"contain"`
+	Selector string                   `json:"selector"`
+	Finds    []string                 `json:"finds"`
+	Type     string                   `json:"type"`
+	SubFinds []string                 `json:"sub_finds"`
+	Keys     map[string]*FilterParams `json:"keys"`
+	Last     bool                     `json:"last"`
+	First    bool                     `json:"first"`
+	Eq       int                      `json:"eq"`
+	HasClass string                   `json:"has_class"`
+	Attr     string                   `json:"attr"`
+	Split    *Split                   `json:"split"`
+	Contains []string                 `json:"contains"`
+	Deletes  []string                 `json:"deletes"`
+	Replaces []*Replace               `json:"replaces"`
 }
 
 type Split struct {
-	Keys  []string
 	Key   string `json:"key"`
 	Index int    `json:"index"`
 }
@@ -46,27 +39,13 @@ func ParseHtml(html string, params map[string]*FilterParams) (res map[string]int
 		return res, err
 	}
 	for k, v := range params {
-		if k == "manual" {
-
-		} else {
-			data := content(dom.Selection, v)
-			dataStr, ok := data.(*string)
-			if ok {
-				if data != "" {
-					res[k] = *dataStr
-				}
-			} else {
-				if data != "" {
-					res[k] = data
-				}
-			}
-		}
+		res[k] = content(dom.Selection, v)
 	}
 
 	return res, err
 }
 
-func finds(s *goquery.Selection, finds []string) *goquery.Selection {
+func finds(finds []string, s *goquery.Selection) *goquery.Selection {
 	if len(finds) > 0 {
 		for i := 0; i < len(finds); i++ {
 			find := finds[i]
@@ -76,42 +55,54 @@ func finds(s *goquery.Selection, finds []string) *goquery.Selection {
 	return s
 }
 
-func selection(s *goquery.Selection, selector string) *goquery.Selection {
-	if selector != "" {
-		s.Find(selector)
-	}
-	return s
-}
-
-func content(dom *goquery.Selection, params *FilterParams, index ...int) (ins interface{}) {
+func content(dom *goquery.Selection, params *FilterParams) (ins interface{}) {
 	s := dom
 	text := ""
 
-	s = selection(s, params.Selector)
-
-	s = finds(s, params.Finds)
-
-	if params.Type == "contains" {
-		text = contains(s, params)
-		return text
+	if params.Selector != "" {
+		s.Find(params.Selector)
 	}
+
+	s = finds(params.Finds, s)
 
 	if params.Type == "list" {
 		resList := make([]interface{}, 0, 10)
 		s.Each(func(i int, ss *goquery.Selection) {
+			ss = finds(params.SubFinds, ss)
 			res := make(map[string]interface{})
 			if params.Keys != nil {
 				for k, v := range params.Keys {
-					res[k] = content(ss, v)
+					if params.HasClass != "" {
+						hasClass := ss.HasClass(params.HasClass)
+						if hasClass {
+							res[k] = content(ss, v)
+						}
+					} else {
+						res[k] = content(ss, v)
+					}
 				}
-				resList = append(resList, res, i)
+				resList = append(resList, res)
 			} else {
-				r := content(ss, &FilterParams{
-					Deletes:  params.Deletes,
-					Replaces: params.Replaces,
-				})
 
-				resList = append(resList, r, i)
+				if params.HasClass != "" {
+					hasClass := ss.HasClass(params.HasClass)
+					if hasClass {
+						r := content(ss, &FilterParams{
+							Deletes:  params.Deletes,
+							Replaces: params.Replaces,
+						})
+
+						resList = append(resList, r)
+					}
+				} else {
+					r := content(ss, &FilterParams{
+						Deletes:  params.Deletes,
+						Replaces: params.Replaces,
+					})
+
+					resList = append(resList, r)
+				}
+
 			}
 
 		})
@@ -120,50 +111,73 @@ func content(dom *goquery.Selection, params *FilterParams, index ...int) (ins in
 
 	s = lastFirstEq(s, params)
 
-	if !s.HasClass(params.HasClass) {
-		return ""
-	}
-
 	text = getText(s, params)
 
-	text = clear(text, params)
+	if params.Split != nil {
+		text = strings.Split(text, params.Split.Key)[params.Split.Index]
+	}
+
+	if len(params.Contains) > 0 {
+		for i := 0; i < len(params.Contains); i++ {
+			contain := params.Contains[i]
+			if !strings.Contains(text, contain) {
+				return ""
+			}
+		}
+	}
+
+	if len(params.Deletes) > 0 {
+		for i := 0; i < len(params.Deletes); i++ {
+			curDelete := params.Deletes[i]
+			if curDelete == "\\n" {
+				text = strings.ReplaceAll(text, "\n", "")
+			} else if curDelete == "\\t" {
+				text = strings.ReplaceAll(text, "\t", "")
+			} else {
+				text = strings.ReplaceAll(text, curDelete, "")
+			}
+		}
+	}
+
+	if len(params.Replaces) > 0 {
+		for i := 0; i < len(params.Replaces); i++ {
+			rep := params.Replaces[i]
+			if rep.Before == "\\n" {
+				text = strings.ReplaceAll(text, "\n", rep.After)
+			} else if rep.Before == "\\t" {
+				text = strings.ReplaceAll(text, "\t", rep.After)
+			} else {
+				text = strings.ReplaceAll(text, rep.Before, rep.After)
+			}
+		}
+	}
 	return text
 }
 
-func contains(s *goquery.Selection, params *FilterParams) (text string) {
-	if len(params.TowFinds) == 0 {
-		s.Each(func(i int, ss *goquery.Selection) {
-			r := content(s, &FilterParams{
-				Deletes:  params.Deletes,
-				Replaces: params.Replaces,
-				Contains: params.Contains,
-			})
-			rr, ok := r.(*string)
-			if ok {
-				if *rr != "" {
-					text = *rr
-				}
-			}
-		})
-	} else {
-		s.Each(func(i int, ss *goquery.Selection) {
-			ss = finds(ss, params.TowFinds)
-			lable := ss.Text()
-			if strings.Contains(lable, params.Contains) {
-				r := content(s, &FilterParams{
-					Finds:    params.ThreeFinds,
-					Deletes:  params.Deletes,
-					Replaces: params.Replaces,
-				})
-				rr, ok := r.(*string)
-				if ok {
-					if *rr != "" {
-						text = *rr
-					}
-				}
-			}
-		})
+func lastFirstEq(s *goquery.Selection, params *FilterParams) *goquery.Selection {
+	if params.Last {
+		s = s.Last()
 	}
 
+	if params.First {
+		s = s.First()
+	}
+
+	if params.Eq != 0 {
+		s = s.Eq(params.Eq)
+	}
+	return s
+}
+
+func getText(s *goquery.Selection, params *FilterParams) (text string) {
+	if params.Attr != "" {
+		ok := false
+		text, ok = s.Attr(params.Attr)
+		if !ok {
+			return ""
+		}
+	} else {
+		text = s.Text()
+	}
 	return text
 }
